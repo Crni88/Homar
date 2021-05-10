360a1a681a59a25062c17722f963c8c92a0374ea
Tarik Kahrić <tarik.kahric@edu.fit.ba>
2021-05-10 09:08:46 +0200

    Fixed NPC to Player dialog

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -16,20 +16,36 @@ public class DialogManager : MonoBehaviour
 
     public Text npcName;
     public Text npcDialogBox;
-    public Text PlayerResponse;
+    public Text playerResponse;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        print("Doslo je do dijaloga");
         dialogUI.SetActive(false);
     }
     private void OnMouseOver()
     {
         distance = Vector3.Distance(player.transform.position, this.transform.position);
-        if (distance<=2.5f)
+        if (distance<=20f)
         {
+            //if (Input.GetAxisRaw("Mouse ScrollWheel") > 0)
+            //{
+            //    currentResponseTracker++;
+            //    if (currentResponseTracker >= npc.playerDialogue.Length-1)
+            //    {
+            //        currentResponseTracker = npc.playerDialogue.Length - 1;
+            //    }
+            //}
+            //else if (Input.GetAxisRaw("Mouse ScrollWheel") > 0)
+            //{
+            //    currentResponseTracker--;
+            //    if (currentResponseTracker<0)
+            //    {
+            //        currentResponseTracker = 0; 
+            //    }
+            //}
+
             //Trigger dialogue
             if (Input.GetKeyDown(KeyCode.E) && isTalking == false)
             {
@@ -38,6 +54,23 @@ public class DialogManager : MonoBehaviour
             else if (Input.GetKeyDown(KeyCode.E) && isTalking == true)
             {
                 EndDialog();
+            }
+            if (currentResponseTracker == 0 && npc.playerDialogue.Length >= 0)
+            {
+                playerResponse.text = npc.playerDialogue[0];
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    npcDialogBox.text = npc.dialogue[1];
+                    currentResponseTracker++;
+                }
+            }
+            else if (currentResponseTracker == 1 && npc.playerDialogue.Length >= 1)
+            {
+                playerResponse.text = npc.playerDialogue[1];
+                //if (Input.GetKeyDown(KeyCode.Return))
+                //{
+                //    npcDialogBox.text = npc.dialogue[2];
+                //}
             }
         }
     }
@@ -49,14 +82,16 @@ public class DialogManager : MonoBehaviour
         dialogUI.SetActive(true);
         npcName.text = npc.npcName;
         npcDialogBox.text = npc.dialogue[0];
-        print("Doslo je do dijaloga");
-
+        print("Pocetak dijaloga...");
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void EndDialog()
     {
         isTalking = false;
         dialogUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+        print("Kraj dijaloga...");
     }
 
 }
